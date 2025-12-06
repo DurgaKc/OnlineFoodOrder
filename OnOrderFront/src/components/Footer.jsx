31401d734517ca42152bcce10ad08fb21c58d8a9
DurgaKC <kcdurga732@gmail.com>
2025-12-06 12:22:41 +0545

    footer

--- OnOrderFront/src/components/Footer.jsx
+++ OnOrderFront/src/components/Footer.jsx
@@ -2,12 +2,27 @@ import { Typography } from "@mui/material";
 
 export default function Footer() {
   return (
-    <footer className="bg-gray-800 text-white py-4 w-full mt-auto">
-      <div className="max-w-7xl mx-auto flex justify-center">
-        <Typography variant="body1" className="text-gray-300 text-center">
-          Copyright© {new Date().getFullYear()}, SKYDECK RESTRO & BAR, ALL RIGHTS RESERVED.
-        </Typography>
-      </div>
-    </footer>
+   <footer className="bg-gray-800 text-white py-4 w-full mt-auto">
+  <div className="max-w-7xl mx-auto flex justify-between items-center px-4">
+
+    {/* Left Side */}
+    <Typography variant="body1" className="text-gray-300">
+      Copyright© {new Date().getFullYear()}, SKYDECK RESTRO & BAR, ALL RIGHTS RESERVED.
+    </Typography>
+
+    {/* Right Side */}
+    <p className="text-gray-300">
+      Developed &amp; Managed By{" "}
+      <a
+        href="https://www.linkedin.com/in/durga-khanal/"
+        className="text-red-500 hover:underline"
+      >
+        @DurgaKhanal
+      </a>
+    </p>
+
+  </div>
+</footer>
+
   );
 }
